e0e15ae7b75b0bc1ebe18ffdbd085c233f084041
Nicole Moreschi <Nicole@Nicole.local>
2020-09-20 15:24:45 +0200

    es_1 completo

--- sketch.js
+++ sketch.js
@@ -4,9 +4,160 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth,windowHeight)
-  // put setup code here
+  background("black");
+  angleMode(DEGREES);
 }
 
 function draw() {
-  // put drawing code here
+
+//1
+  noFill();
+  stroke(lerpColor(color("black"),color("#FF3838"),frameCount/180));
+
+  push();
+  translate(width/6, width/6);
+  rotate(frameCount * 2);
+  translate(width/12, 0);
+  ellipse(0, 0, width/6);
+  pop();
+
+  if(frameCount == 180){
+    noLoop();
+  }
+
+  //2b
+  noFill();
+  stroke(lerpColor(color("black"),color("#FF3838"),frameCount/180));
+
+  push();
+  translate(width/6 + width/6, width/6 + width/6);
+  rotate(frameCount * 2);
+  translate(width/12, 0);
+  ellipse(0, 0, width/6);
+  pop();
+
+  if(frameCount == 180){
+  noLoop();
+  }
+
+//2
+  noFill();
+  stroke(lerpColor(color("black"),color("#FF3838"),frameCount/180));
+
+  push();
+  translate(width/6 + width/6, width/6);
+  rotate(-frameCount * 2);
+  translate(width/12, 0);
+  ellipse(0, 0, width/6);
+  pop();
+
+  if(frameCount == 180){
+  noLoop();
+  }
+
+  //3
+  noFill();
+  stroke(lerpColor(color("black"),color("#FF3838"),frameCount/180));
+
+  push();
+  translate(width/6 + 2*width/6, width/6);
+  rotate(frameCount * 2);
+  translate(width/12, 0);
+  ellipse(0, 0, width/6);
+  pop();
+
+  if(frameCount == 180){
+  noLoop();
+  }
+
+  //4b
+  noFill();
+  stroke(lerpColor(color("black"),color("#FF3838"),frameCount/180));
+
+  push();
+  translate(width/6 + 3*width/6, width/6 + width/6);
+  rotate(frameCount * 2);
+  translate(width/12, 0);
+  ellipse(0, 0, width/6);
+  pop();
+
+  if(frameCount == 180){
+  noLoop();
+  }
+
+  //4
+  noFill();
+  stroke(lerpColor(color("black"),color("#FF3838"),frameCount/180));
+
+  push();
+  translate(width/6 + 3*width/6, width/6);
+  rotate(-frameCount * 2);
+  translate(width/12, 0);
+  ellipse(0, 0, width/6);
+  pop();
+
+  if(frameCount == 180){
+  noLoop();
+  }
+
+  //5
+  noFill();
+  stroke(lerpColor(color("black"),color("#FF3838"),frameCount/120));
+
+  push();
+  translate(width/6 + 4*width/6, width/6);
+  rotate(frameCount * 2);
+  translate(width/12, 0);
+  ellipse(0, 0, width/6);
+  pop();
+
+  if(frameCount == 180){
+  noLoop();
+  }
+
+  //1b
+  noFill();
+  stroke(lerpColor(color("black"),color("#FF3838"),frameCount/120));
+
+  push();
+  translate(width/6, width/6 + width/6);
+  rotate(- frameCount * 2);
+  translate(width/12, 0);
+  ellipse(0, 0, width/6);
+  pop();
+
+  if(frameCount == 180){
+  noLoop();
+  }
+
+  //3b
+  noFill();
+  stroke(lerpColor(color("black"),color("#FF3838"),frameCount/120));
+
+  push();
+  translate(width/6 + 2*width/6, width/6 + width/6);
+  rotate(-frameCount * 2);
+  translate(width/12, 0);
+  ellipse(0, 0, width/6);
+  pop();
+
+  if(frameCount == 180){
+  noLoop();
+  }
+
+  //5b
+  noFill();
+  stroke(lerpColor(color("black"),color("#FF3838"),frameCount/120));
+
+  push();
+  translate(width/6 + 4*width/6, width/6 + width/6);
+  rotate(-frameCount * 2);
+  translate(width/12, 0);
+  ellipse(0, 0, width/6);
+  pop();
+
+  if(frameCount == 180){
+  noLoop();
+  }
+
 }
